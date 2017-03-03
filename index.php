26ba94f2f0092e7f3c2cc08c1ddee5ba8987c6a3
GZOZO94 <gaszol11.c@gmail.com>
2017-03-03 22:28:05 +0100

    1.0

--- index.php
+++ index.php
@@ -1,9 +1,58 @@
-<html>
-	<head>
-		<title>	
-		</title>
-	</head>
-	<body>
+<!DOCTYPE HTML>
+<html lang="hu">
+<head>
+	<meta http-equiv="Content_Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>The future software</title>
+	<link href="Pictures/logo.ico" rel="shortcut icon">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="Java/my_jquery.js"></script>
+	<link rel="stylesheet" href="Style/style.css" type="text/css">
+	<script>
+	
+	$(document).ready(function(){
+		$(".navbar-brand").click(function(e){
+			e.preventDefault();
+			var destId=e.currentTarget.id+"Section";
 
-	</body>
+			$("html, body").animate({
+				scrollTop: $("#"+destId).offset().top
+			},1000);
+		});
+	});
+	</script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" data-target="#hide" data-toggle="collapse" class="navbar-toggle">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a id="about" href="#aboutSection" class="navbar-brand">The Future Software</a>
+			</div>
+			<div class="collapse navbar-collapse" id="hide">
+				<ul class="nav navbar-nav">
+				
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					
+				</ul>
+			</div>
+		</div>
+	</nav>	
+	<div id="aboutSection" class="container-fluid">
+		<h2>Rólunk <small>The Future Software</small></h2>
+		<blockquote class="well text-center">
+			
+		</blockquote>
+	</div>
+	<div class="container-fluid text-center">
+		<p>Copyright &copy; The Smart Gardener 2017</p>
+	</div>
+</body>
 </html>
