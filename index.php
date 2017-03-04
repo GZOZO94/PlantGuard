6f14df0b6c6242d26189795ddd97f2956d6e8cb9
GZOZO94 <gaszol11.c@gmail.com>
2017-03-04 21:46:45 +0100

    1.6

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 	<link href="Pictures/logo.ico" rel="shortcut icon">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="animation.css">
+	<link rel="stylesheet" href="demo.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="transitions.js"></script>
 	<script>
 	
 	$(document).ready(function(){
@@ -35,6 +38,8 @@
 			</div>
 		</div>
 	</nav>	
+	<article class="kontext">
+		<div class="layer one show">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -74,5 +79,66 @@
 	<div class="container-fluid text-center">
 		<p>Copyright &copy; The Smart Gardener 2017</p>
 	</div>
+	</div>
+	<div class="layer two">
+		<h1>Adatok</h1>
+	</div>
+	</article>
+	<ul class="bullets"></ul>
+	
+	<script>
+		$(document).ready(function(){
+			// Create a new instance of kontext
+			var k = kontext( document.querySelector( '.kontext' ) );
+
+			var bulletsContainer = document.body.querySelector( '.bullets' );
+
+			// Create one bullet per layer
+			for( var i = 0, len = k.getTotal(); i < len; i++ ) {
+				var bullet = document.createElement( 'li' );
+				bullet.className = i === 0 ? 'active' : '';
+				bullet.setAttribute( 'index', i );
+				bullet.onclick = function( event ) { k.show( event.target.getAttribute( 'index' ) ) };
+				bullet.ontouchstart = function( event ) { k.show( event.target.getAttribute( 'index' ) ) };
+				bulletsContainer.appendChild( bullet );
+			}
+
+			// Update the bullets when the layer changes
+			k.changed.add( function( layer, index ) {
+				var bullets = document.body.querySelectorAll( '.bullets li' );
+				for( var i = 0, len = bullets.length; i < len; i++ ) {
+					bullets[i].className = i === index ? 'active' : '';
+				}
+			} );
+
+			document.addEventListener( 'keyup', function( event ) {
+				if( event.keyCode === 37 ) k.prev();
+				if( event.keyCode === 39 ) k.next();
+			}, false );
+
+			var touchX = 0;
+			var touchConsumed = false;
+
+			document.addEventListener( 'touchstart', function( event ) {
+				touchConsumed = false;
+				lastX = event.touches[0].clientX;
+			}, false );
+
+			document.addEventListener( 'touchmove', function( event ) {
+				event.preventDefault();
+
+				if( !touchConsumed ) {
+					if( event.touches[0].clientX > lastX + 10 ) {
+						k.prev();
+						touchConsumed = true;
+					}
+					else if( event.touches[0].clientX < lastX - 10 ) {
+						k.next();
+						touchConsumed = true;
+					}
+				}
+			}, false );
+			});
+		</script>
 </body>
 </html>
