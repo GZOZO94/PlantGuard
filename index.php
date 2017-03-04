ab8121f8b98bf3fa04eefd145ab7b5045800d5d6
GZOZO94 <gaszol11.c@gmail.com>
2017-03-04 22:15:42 +0100

    1.7

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 	<meta http-equiv="Content_Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>The Smart Gardener</title>
-	<link href="Pictures/logo.ico" rel="shortcut icon">
+	<link href="Pictures/l.ico" rel="shortcut icon">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<link rel="stylesheet" href="animation.css">
-	<link rel="stylesheet" href="demo.css">
+	<link href='http://fonts.googleapis.com/css?family=Playfair+Display:900,400|Lato:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/dragdealer.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="transitions.js"></script>
+	<script src="modernizr.custom.js"></script>
 	<script>
 	
 	$(document).ready(function(){
@@ -26,20 +29,14 @@
 	</script>
 </head>
 <body>
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" data-target="#hide" data-toggle="collapse" class="navbar-toggle">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a id="about" href="#aboutSection" class="navbar-brand">The Smart Gardener</a>
-			</div>
-		</div>
-	</nav>	
-	<article class="kontext">
-		<div class="layer one show">
+	<header id="header" class="codrops-header">
+		<h1>The Smart Gardener</h1>
+		<button class="slider-switch">Switch view</button>
+	</header>
+	<div id="slideshow" class="dragslider">
+				<section class="img-dragger img-dragger-large dragdealer">
+					<div class="handle">
+						<div class="slide" data-content="content-1">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -79,66 +76,64 @@
 	<div class="container-fluid text-center">
 		<p>Copyright &copy; The Smart Gardener 2017</p>
 	</div>
-	</div>
-	<div class="layer two">
-		<h1>Adatok</h1>
-	</div>
-	</article>
-	<ul class="bullets"></ul>
-	
-	<script>
-		$(document).ready(function(){
-			// Create a new instance of kontext
-			var k = kontext( document.querySelector( '.kontext' ) );
+				</div>
+		</section>
+	<section class="pages">
+					<div class="content" data-content="content-1">
+						<h1>Adatok</h1>
+					</div>
+				</section>
+			</div>
+		<script src="dragdealer.js"></script>
+		<script src="classie.js"></script>
+		<script src="dragslideshow.js"></script>
+		<script>
+			$(document).ready(function() {
 
-			var bulletsContainer = document.body.querySelector( '.bullets' );
+				var overlay = $( 'overlay' ),
+					overlayClose = overlay.querySelector( 'button' ),
+					header = $( 'header' )
+					switchBtnn = header.querySelector( 'button.slider-switch' ),
+					toggleBtnn = function() {
+						if( slideshow.isFullscreen ) {
+							classie.add( switchBtnn, 'view-maxi' );
+						}
+						else {
+							classie.remove( switchBtnn, 'view-maxi' );
+						}
+					},
+					toggleCtrls = function() {
+						if( !slideshow.isContent ) {
+							classie.add( header, 'hide' );
+						}
+					},
+					toggleCompleteCtrls = function() {
+						if( !slideshow.isContent ) {
+							classie.remove( header, 'hide' );
+						}
+					},
+					slideshow = new DragSlideshow( $('slideshow' ), { 
+						// toggle between fullscreen and minimized slideshow
+						onToggle : toggleBtnn,
+						// toggle the main image and the content view
+						onToggleContent : toggleCtrls,
+						// toggle the main image and the content view (triggered after the animation ends)
+						onToggleContentComplete : toggleCompleteCtrls
+					}),
+					toggleSlideshow = function() {
+						slideshow.toggle();
+						toggleBtnn();
+					},
+					closeOverlay = function() {
+						classie.add( overlay, 'hide' );
+					};
 
-			// Create one bullet per layer
-			for( var i = 0, len = k.getTotal(); i < len; i++ ) {
-				var bullet = document.createElement( 'li' );
-				bullet.className = i === 0 ? 'active' : '';
-				bullet.setAttribute( 'index', i );
-				bullet.onclick = function( event ) { k.show( event.target.getAttribute( 'index' ) ) };
-				bullet.ontouchstart = function( event ) { k.show( event.target.getAttribute( 'index' ) ) };
-				bulletsContainer.appendChild( bullet );
-			}
+				// toggle between fullscreen and small slideshow
+				switchBtnn.addEventListener( 'click', toggleSlideshow );
+				// close overlay
+				overlayClose.addEventListener( 'click', closeOverlay );
 
-			// Update the bullets when the layer changes
-			k.changed.add( function( layer, index ) {
-				var bullets = document.body.querySelectorAll( '.bullets li' );
-				for( var i = 0, len = bullets.length; i < len; i++ ) {
-					bullets[i].className = i === index ? 'active' : '';
-				}
-			} );
-
-			document.addEventListener( 'keyup', function( event ) {
-				if( event.keyCode === 37 ) k.prev();
-				if( event.keyCode === 39 ) k.next();
-			}, false );
-
-			var touchX = 0;
-			var touchConsumed = false;
-
-			document.addEventListener( 'touchstart', function( event ) {
-				touchConsumed = false;
-				lastX = event.touches[0].clientX;
-			}, false );
-
-			document.addEventListener( 'touchmove', function( event ) {
-				event.preventDefault();
-
-				if( !touchConsumed ) {
-					if( event.touches[0].clientX > lastX + 10 ) {
-						k.prev();
-						touchConsumed = true;
-					}
-					else if( event.touches[0].clientX < lastX - 10 ) {
-						k.next();
-						touchConsumed = true;
-					}
-				}
-			}, false );
-			});
+			}());
 		</script>
 </body>
 </html>
